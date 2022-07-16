23c30a0f16959e2444272bc4d0abee7d4edec3fe
Gnwel <coolthr@gmx.com>
2022-07-15 22:54:12 -0500

    committing current changes

---
+++ Assets/Scripts/Dice_Peanut.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice_Peanut : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool CheckSides()
+    {
+        if(transform.up == 0,1,0)
+        Debug.Log("Detected");
+        /*
+        //z
+        transform.forward;
+
+        -transform.forward;
+
+        transform.up;
+
+        -transform.up;
+
+        transform.right;
+
+        transform.left;
+*/return false;
+    }
+    
+}
